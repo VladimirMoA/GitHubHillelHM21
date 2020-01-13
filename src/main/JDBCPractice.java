5fe8d22fd75be021b149b55f400e9c97d2425145
VladimirMoA <51534048+VladimirMoA@users.noreply.github.com>
2020-01-13 18:55:33 +0200

    Update JDBCPractice.java

--- src/main/JDBCPractice.java
+++ src/main/JDBCPractice.java
@@ -1,1 +1,16 @@
-Hellooooooooo
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/student?"
+                        + "useSSL=false&user=root&password=root");
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select id, city from CITY");
